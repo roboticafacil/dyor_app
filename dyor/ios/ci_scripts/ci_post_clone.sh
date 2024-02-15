0abc78a6d980ceaeb40be00e56e8bc212a63e7e6
roboticafacil <roboticafacil.es@gmail.com>
2024-02-15 08:38:31 +0100

    modified:   ios/ci_scripts/ci_post_clone.sh

--- dyor/ios/ci_scripts/ci_post_clone.sh
+++ dyor/ios/ci_scripts/ci_post_clone.sh
@@ -1,19 +1,16 @@
 #!/bin/sh
 
+# Fail this script if any subcommand fails.
+set -e
+
 # The default execution directory of this script is the ci_scripts directory.
 cd $CI_WORKSPACE # change working directory to the root of your cloned repo.
 
-# Install CocoaPods using Homebrew.
-HOMEBREW_NO_AUTO_UPDATE=1 # disable homebrew's automatic updates.
-
-# Install CocoaPods using Homebrew.
-brew install cocoapods
-
-# Install Flutter
+# Install Flutter using git.
 git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
 export PATH="$PATH:$HOME/flutter/bin"
 
-# Install Flutter artifacts for iOS (--ios)
+# Install Flutter artifacts for iOS (--ios), or macOS (--macos) platforms.
 flutter precache --ios
 
 # Install Flutter dependencies.
@@ -21,5 +18,12 @@ flutter pub get
 
 # Build ios app
 flutter build ios --no-codesign
+
+# Install CocoaPods using Homebrew.
+HOMEBREW_NO_AUTO_UPDATE=1 # disable homebrew's automatic updates.
+brew install cocoapods
+
+# Install CocoaPods dependencies.
+cd ios && pod install # run `pod install` in the `ios` directory.
 
 exit 0
